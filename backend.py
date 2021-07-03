086365abdc2e227a1b9b56fcfee43a1ef9091426
AndyTsai <44059186+andy1000335@users.noreply.github.com>
2021-07-03 21:31:14 +0800

    Remove pwd

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ def connectDatabase():
         'host': '127.0.0.1', 
         'port': 3306, 
         'user': 'root', 
-        'password': 'jb1000335', 
+        'password': 'password', 
         'db': 'OBS', 
         'charset': 'utf8'
     }
